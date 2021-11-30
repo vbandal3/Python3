fe8cbcd66818f2267f54765b05b2df96fe6a759a
Vaibhav Bandal <93034627+vbandal3@users.noreply.github.com>
2021-11-30 08:05:01 +0530

    variables , datatypes and datacasting

---
+++ variables,datatypes and datacasting.py
@@ -0,0 +1,21 @@
+"""
+str()
+int()
+float()
+var(n)
+"""
+var1 = "India "
+var2 = 50
+var3 = 45.3
+var4 = "great"
+print(var2+var3)#integer & float addition possible
+print(var1 + var4)# string additio is possible
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+var5 = "55"
+var6 = "43"
+print(int(var5)+int(var6))
+print(20 * str(int(var5)+int(var6) ) )
+print(5* "india \n ")
